66039e0c4820661591ad039a644357999c408266
John Stanford on deathStar <johnwstanford@gmail.com>
2020-10-19 23:25:00 -0500

    Moved simple allocation and deallocation from example to a test; added memcpy to test

--- examples/01_hello_world.rs
+++ examples/01_hello_world.rs
@@ -1,19 +1,5 @@
 
-use cufft_rs::runtime;
-
 fn main() -> Result<(), &'static str> {
-
-	let mut ptr:usize = 0;
-
-	match unsafe { runtime::cudaMalloc(&mut ptr, std::mem::size_of::<f32>()) } {
-		0 => (),
-		_ => return Err("Unable to allocate memory")
-	};
-
-	match unsafe { runtime::cudaFree(ptr) } {
-		0 => (),
-		_ => return Err("Unable to free memory")
-	}
 
 	Ok(())
 
--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,10 +1,45 @@
 
 use libc::size_t;
 
+#[cfg(test)]
+mod tests;
+
+#[repr(C)]
+pub enum CudaMemCopyKind {
+	HostToHost     = 0,
+	HostToDevice   = 1,
+	DeviceToHost   = 2,
+	DeviceToDevice = 3,
+	Default        = 4		// Inferred from pointer types, requires unified virtual addressing
+}
+
 #[link(name = "cudart")]
 extern {
 
-	pub fn cudaMalloc(ptr:&mut size_t, size:size_t) -> i32;
-	pub fn cudaFree(ptr:size_t) -> i32;
+	// Use size_t for pointers to device memory because it's simpler and also because it's an extra safeguard against
+	// dereferencing them, even in unsafe blocks
+
+	fn cudaMalloc(ptr:&mut size_t, size:size_t) -> i32;
+	fn cudaMemcpy(dst:*mut u8, src:*const u8, count:size_t, kind:CudaMemCopyKind ) -> i32;
+	fn cudaFree(ptr:size_t) -> i32;
 
 }
+
+unsafe fn memcpy_to_device<T: Sized>(dst:size_t, src:&[T]) -> Result<(), &'static str> {
+	let src_ptr:*const T     = &src[0];
+	let src_ptr_u8:*const u8 = src_ptr as *const _;
+	match cudaMemcpy(dst as *mut u8, src_ptr_u8, std::mem::size_of::<T>()*src.len(), CudaMemCopyKind::HostToDevice) {
+		0 => Ok(()),
+		_ => Err("Unable to copy memory to device")
+	}
+}
+
+unsafe fn memcpy_from_device<T: Sized>(dst:&mut [T], src:size_t) -> Result<(), &'static str> {
+	let dst_ptr:*mut T     = &mut dst[0];
+	let dst_ptr_u8:*mut u8 = dst_ptr as *mut _;
+	match cudaMemcpy(dst_ptr_u8, src as *const u8, std::mem::size_of::<T>()*dst.len(), CudaMemCopyKind::DeviceToHost) {
+		0 => Ok(()),
+		_ => Err("Unable to copy memory from device")
+	}
+}
+
